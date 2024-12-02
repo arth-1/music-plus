4aeabd1ae887fa09e27102a225c86b10e796b01f
arth-1 <arthag1304@gmail.com>
2024-12-03 00:12:26 +0530

    error correction

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -3,14 +3,23 @@
 import qs from "query-string"
 
 import useDebounce from "@/hooks/useDebounce"
-import { useRouter } from "next/router"
+import { useRouter } from "next/compat/router"
 import { useEffect, useState } from "react"
 import Input from "./Input"
 
 const SearchInput = () => {
+
+    
     const router = useRouter()
     const [value, setValue] = useState<string>("")
     const debouncedValue = useDebounce<string>(value, 500)
+
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
 
     useEffect(() => {
         const query = {
@@ -22,8 +31,10 @@ const SearchInput = () => {
           query,
         })
     
-        router.push(url)
-      }, [debouncedValue, router])
+        if (router && router.push) {
+            router.push(url);
+        }
+    }, [debouncedValue, router, isMounted])
 
     return (
         <Input placeholder="What do you want to listen to?" value={value} onChange={e => setValue(e.target.value)} />
